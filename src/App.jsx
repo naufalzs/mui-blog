6fa4f420d1ef5a99a60fb3b8c304bb0cabf5a461
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-10-05 11:18:55 +0700

    feat: setup main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import { Box } from "@mui/material";
+import { CssBaseline, Grid } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
 
 function App() {
   return (
     <>
-      <Box>MUI Blog</Box>
+      <CssBaseline>
+        <Navbar />
+        <Grid container>
+          <Grid item xs>
+            <Sidebar />
+          </Grid>
+          <Grid item xs>
+            <Feed />
+          </Grid>
+          <Grid item xs>
+            <Rightbar />
+          </Grid>
+        </Grid>
+      </CssBaseline>
     </>
   );
 }
